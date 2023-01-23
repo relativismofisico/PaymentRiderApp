aa4d94b5a519d1f1751fb37ab7cbd7f2f89a6728
Redondo <relativismofisico@gmail.com>
2023-01-23 09:09:41 -0500

    Estrutura proyecto y clases

---
+++ src/main/java/com/payment/rider/driver/PaymentRider/PaymentRiderApplication.java
@@ -0,0 +1,13 @@
+package com.payment.rider.driver.PaymentRider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentRiderApplication.class, args);
+	}
+
+}
