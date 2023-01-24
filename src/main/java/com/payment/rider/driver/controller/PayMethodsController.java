a0bf726136b1f2eff6dd45bf8149b21239f95988
Redondo <relativismofisico@gmail.com>
2023-01-24 13:28:43 -0500

    Implementacion de los controller

---
+++ src/main/java/com/payment/rider/driver/controller/PayMethodsController.java
@@ -0,0 +1,84 @@
+package com.payment.rider.driver.controller;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.payment.rider.driver.dao.PayMethodDao;
+import com.payment.rider.driver.dto.PayMethodDTO;
+import com.payment.rider.driver.entity.PayMethod;
+import com.payment.rider.driver.entity.User;
+import com.payment.rider.driver.services.IPayMethods;
+import com.payment.rider.driver.services.IPaymentGateway;
+import com.payment.rider.driver.services.IUser;
+
+@RestController
+@RequestMapping("/api/pays")
+public class PayMethodsController {
+
+	@Autowired
+	private IPaymentGateway iPaymentGateway;
+	
+	@Autowired
+	private IUser iUser;
+	
+	@Autowired
+	private IPayMethods iPayMethods;
+	
+	@GetMapping("/Status/transaction/{transactionId}")
+	public String getStatusTransaction(@PathVariable String transactionId){
+		
+		String statusTransaction = iPaymentGateway.statusTransaction(transactionId);
+		
+		return "The transaction is: " + statusTransaction;
+	}
+	
+	@SuppressWarnings("unused")
+	@PostMapping("/{userId}")
+	public String createdPayMethod(@Valid @RequestBody PayMethodDTO payMethodDto, BindingResult result, @PathVariable Integer userId) {
+		
+		String response= "";
+		
+		User user=null;
+		
+		try {
+			if(result.hasErrors()) {
+				throw new RuntimeException("There is some Error.........Try again");
+			}
+			
+			user=iUser.getUser(userId);
+			
+			if(user.equals(null)) {
+				throw new RuntimeException("User doesn't exist.........Try again");
+
+			}
+			
+			if(!user.getRole().equals("rider")) {
+				throw new RuntimeException("User isn't a Rider.........Try again");
+			}
+			
+			JsonNode payMethodDTOCreated = iPaymentGateway.createPaySource(payMethodDto);
+			PayMethod payMethod = iPayMethods.savePayMethod(payMethodDto, payMethodDTOCreated.get("id").asInt()
+					, payMethodDTOCreated.get("status").asText(), user);
+		
+			response = "Number's Pay is: " + payMethodDTOCreated.get("id").asText();
+			
+		} catch (Exception e) {
+			response= e.getMessage();
+		}
+		
+		return response;
+		
+		
+	}
+	
+}
--- src/main/java/com/payment/rider/driver/controller/RiderController.java
+++ src/main/java/com/payment/rider/driver/controller/RiderController.java
@@ -1,5 +1,7 @@
 package com.payment.rider.driver.controller;
 
+import java.util.Map;
+
 import javax.management.RuntimeErrorException;
 import javax.naming.Binding;
 import javax.validation.Valid;
@@ -9,21 +11,27 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.payment.rider.driver.dao.LocationDao;
 import com.payment.rider.driver.dto.LocationDTO;
+import com.payment.rider.driver.dto.TransactionDTO;
 import com.payment.rider.driver.entity.Location;
 import com.payment.rider.driver.entity.Rider;
+import com.payment.rider.driver.entity.Transaction;
+import com.payment.rider.driver.entity.User;
 import com.payment.rider.driver.services.ILocation;
 import com.payment.rider.driver.services.IPaymentGateway;
 import com.payment.rider.driver.services.IRider;
 import com.payment.rider.driver.services.ITransaction;
+import com.payment.rider.driver.servicesImpl.PaymentGatewayImpl;
 
 @RestController
-@RequestMapping("api/versionDemo/")
+@RequestMapping("/api/versionDemo")
 public class RiderController {
 	
 	@Autowired
@@ -37,6 +45,9 @@ public class RiderController {
 	
 	@Autowired
 	private ITransaction iTransaction;
+	
+	@Autowired
+	private TransactionDTO transactionDto;
 	
 	@PostMapping(value="/rider/start/{userId}")
 	public String startRider(@Valid @RequestBody LocationDTO location, BindingResult result, @PathVariable Integer userId) {
@@ -60,6 +71,47 @@ public class RiderController {
 		
 		return response;
 		
+	}
+	
+	@PutMapping(value="/rider/end/{userId]/{riderId}")
+	public String endRider(@PathVariable Integer userId, @PathVariable Integer riderId, @RequestBody Map<String, String> data) {
+		
+		String response= "";
+		
+		try {
+			
+			Integer distanceTraveledKm = Integer.valueOf(data.get("km"));
+			Integer time = Integer.valueOf(data.get("minutes"));
+			Integer installments = Integer.valueOf(data.get("installments"));
+			String reference= data.get("reference");
+			
+			if(reference.equals("")) {
+				throw new RuntimeException("We need a reference.....");
+			}
+			
+			transactionDto.setAmountInPesos(distanceTraveledKm, time);
+			transactionDto.setReference(reference);
+			transactionDto.setInstallments(installments);
+			transactionDto.setCurrency("COP");
+			
+			User user = iRider.endRider(userId, riderId);
+			
+			JsonNode transaction = iPaymentGateway.createTransaction(transactionDto, user);
+			
+			Transaction transactionCreated= iTransaction.saveTransaction(transactionDto, transaction.get("id").asText()
+					, user.getRide(),user.getPayMethod());
+			
+			if(transactionCreated.equals(null)) {
+				throw new RuntimeException("Try Again.....");
+			}
+			
+			response = "La transaction is: " + transactionCreated.getId();
+			
+		} catch (Exception e) {
+			response= e.getMessage();
+		}
+		
+		return response;
 	}
 	
 	
